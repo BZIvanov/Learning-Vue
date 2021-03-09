2be8e77c3862f55eb02c16897a795b50caa0b477
BZIvanov <ivanov.biser@gmx.com>
2021-03-09 14:14:33 +0200

    demo with form and emit

---
+++ 03.Components/demo-c/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <section>
+    <article>
+      <active-user :username="user.username" :age="user.age"></active-user>
+    </article>
+    <article>
+      <user-data @update-user="updateData"></user-data>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: { username: 'Iva', age: 23 },
+    };
+  },
+  methods: {
+    updateData(username, age) {
+      this.user.username = username;
+      this.user.age = parseInt(age, 10) || 'unknown';
+    },
+  },
+};
+</script>
+
+<style>
+#app section {
+  display: flex;
+  justify-content: space-evenly;
+}
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 3px solid #ccc;
+  margin: 0 0 1rem 0;
+  min-width: 150px;
+}
+#app button {
+  cursor: pointer;
+  border: 1px solid #ff0177;
+  background-color: #ff0177;
+  color: white;
+  padding: 0.05rem 1rem;
+}
+</style>
---
+++ 03.Components/demo-c/src/components/UserData.vue
@@ -0,0 +1,28 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <input type="text" v-model="username" placeholder="username" />
+    </div>
+    <div>
+      <input type="number" v-model="age" placeholder="age" />
+    </div>
+    <button>Update</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      age: '',
+    };
+  },
+  emits: ['update-user'],
+  methods: {
+    submitData() {
+      this.$emit('update-user', this.username, this.age);
+    },
+  },
+};
+</script>
