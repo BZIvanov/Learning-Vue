cb1ca37f5be316e95a143494f8487d93b48e31be
BZIvanov <biser.z.ivanov@gmail.com>
2021-03-09 23:52:01 +0200

    provide and inject demo

---
+++ 03.Components/demo-d/src/components/KnowledgeElement.vue
@@ -0,0 +1,16 @@
+<template>
+  <li>
+    <h3>{{ title }}</h3>
+    <p>{{ description }}</p>
+    <button @click="selectTopic(id)">Select</button>
+  </li>
+</template>
+
+<script>
+export default {
+  // with inject we can get data or functions from provide property from components up in tree structure
+  inject: ['selectTopic'],
+  props: ['id', 'title', 'description'],
+  emits: ['select-topic'],
+};
+</script>
