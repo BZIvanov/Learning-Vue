f3de10cf83885d044d61bea624088710e299cb38
BZIvanov <biser.z.ivanov@gmail.com>
2021-03-09 12:36:56 +0200

    emitting events demo

---
+++ 03.Components/demo-b/src/components/FriendBasic.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isOnline ? '(Online)' : '(Off)' }}</h2>
+    <button @click="toggleOnline">Toggle Online</button>
+  </li>
+</template>
+
+<script>
+export default {
+  // here we have basic props and emits definitions
+  props: ['id', 'name', 'isOnline'],
+  emits: ['toggle-online'],
+  methods: {
+    toggleOnline() {
+      // the first parameter is the name of the event we are emitting, every next is additional data we want to emit
+      this.$emit('toggle-online', this.id);
+    },
+  },
+};
+</script>
---
+++ 03.Components/demo-b/src/components/FriendDetailed.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isOnline ? '(Online)' : '(Off)' }}</h2>
+    <button @click="toggleOnline">Toggle Online</button>
+  </li>
+</template>
+
+<script>
+export default {
+  // here we have detailed props and emits definitions, with data validation
+  props: {
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    isOnline: { type: Boolean, required: false, default: false },
+  },
+  // emits property only reveals details and adds validation, but is not actually the one emitting the event, emitter is in the methods
+  emits: {
+    'toggle-online': function(id) {
+      if (id) {
+        return true;
+      } else {
+        console.warn('Id is missing');
+        return false;
+      }
+    },
+  },
+  methods: {
+    toggleOnline() {
+      this.$emit('toggle-online', this.id);
+    },
+  },
+};
+</script>
