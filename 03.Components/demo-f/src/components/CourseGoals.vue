cd8603a3af3c8d3068cfbb57a6154c832b22701c
BZIvanov <biser.z.ivanov@gmail.com>
2021-03-10 10:55:26 +0200

    slot props demo

---
+++ 03.Components/demo-f/src/components/CourseGoals.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-for="item in items" :key="item">
+      <!-- we can provide parameters to slots like below -->
+      <slot :item="item" another-prop="xxx"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: ['First', 'Second'],
+    };
+  },
+};
+</script>
