2e852da0d577f039636605c32e577382ffd3f0d1
BZIvanov <biser.z.ivanov@gmail.com>
2021-03-10 16:34:32 +0200

    using http calls demo

---
+++ 05.Http/demo-a/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <learning-survey></learning-survey>
+  <user-experiences></user-experiences>
+</template>
+
+<script>
+import LearningSurvey from './components/survey/LearningSurvey.vue';
+import UserExperiences from './components/survey/UserExperiences.vue';
+
+export default {
+  components: {
+    LearningSurvey,
+    UserExperiences,
+  },
+};
+</script>
