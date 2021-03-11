c8da6af273e34e3ad25dbb626e88e7e2ef05c5d7
BZIvanov <biser.z.ivanov@gmail.com>
2021-03-11 22:32:22 +0200

    navigation demo

---
+++ 06.Navigation/demo-a/src/components/teams/TeamsList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <teams-item
+      v-for="team in teams"
+      :key="team.id"
+      :id="team.id"
+      :name="team.name"
+      :member-count="team.members.length"
+    ></teams-item>
+  </ul>
+</template>
+
+<script>
+import TeamsItem from './TeamsItem.vue';
+
+export default {
+  components: {
+    TeamsItem,
+  },
+  inject: ['teams'],
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 1rem auto;
+  max-width: 39rem;
+  padding: 0;
+}
+</style>
