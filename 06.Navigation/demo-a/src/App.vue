4ca909331030a2e94ad9d6e1fab092a8af95c78b
BZIvanov <ivanov.biser@gmx.com>
2021-03-11 22:32:22 +0200

    navigation demo

---
+++ 06.Navigation/demo-a/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <the-navigation></the-navigation>
+  <main>
+    <router-view></router-view>
+  </main>
+</template>
+
+<script>
+import TheNavigation from './components/nav/TheNavigation.vue';
+
+export default {
+  components: {
+    TheNavigation,
+  },
+  data() {
+    return {
+      teams: [
+        { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
+        { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
+        { id: 't3', name: 'Client Consulting', members: ['u4', 'u5'] },
+      ],
+      users: [
+        { id: 'u1', fullName: 'Petko Petrov', role: 'Engineer' },
+        { id: 'u2', fullName: 'Mira Ivanova', role: 'Engineer' },
+        { id: 'u3', fullName: 'Boqn Trifonov', role: 'Engineer' },
+        { id: 'u4', fullName: 'Magdalena Berova', role: 'Consultant' },
+        { id: 'u5', fullName: 'Ivan Ivanov', role: 'Consultant' },
+      ],
+    };
+  },
+  provide() {
+    return {
+      teams: this.teams,
+      users: this.users,
+    };
+  },
+};
+</script>
---
+++ 06.Navigation/demo-a/src/components/teams/TeamsItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <li>
+    <h3>{{ name }}</h3>
+    <div class="team-members">{{ memberCount }} Members</div>
+    <router-link :to="teamMembersLink">View Members</router-link>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['id', 'name', 'memberCount'],
+  computed: {
+    teamMembersLink() {
+      return '/teams/' + this.id;
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  box-shadow: 0 2px 7px rgba(0, 0, 0, 0.27);
+  border-radius: 10px;
+  padding: 1rem;
+}
+
+li h3 {
+  margin: 0.5rem 0;
+  font-size: 1.25rem;
+}
+
+li .team-members {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+  display: inline-block;
+  padding: 0.5rem 1.3rem;
+  background-color: #11015c;
+}
+
+a:hover,
+a:active {
+  background-color: #221a8d;
+}
+</style>
