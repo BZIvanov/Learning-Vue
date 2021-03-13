4b4748f110a6d00bd159ab3f65c8551006bc6f74
BZIvanov <ivanov.biser@gmx.com>
2021-03-13 16:33:04 +0200

    vuex dispatch and actions demo

---
+++ 08.Vuex/demo-b/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <base-container title="Vuex">
+    <the-number-box></the-number-box>
+    <button @click="addTen">Add 10</button>
+    <change-counter></change-counter>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheNumberBox from './components/TheNumberBox.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheNumberBox,
+    ChangeCounter,
+  },
+  methods: {
+    addTen() {
+      this.$store.dispatch({
+        type: 'increase',
+        value: 10,
+      });
+    },
+  },
+};
+</script>
---
+++ 08.Vuex/demo-b/src/components/ChangeCounter.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="inc">Add 1 delayed</button>
+  <button @click="increase({ value: 11 })">Add 11</button>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  methods: {
+    ...mapActions({
+      inc: 'increment',
+      increase: 'increase',
+    }),
+  },
+};
+</script>
