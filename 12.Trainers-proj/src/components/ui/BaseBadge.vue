dd341fcb566ba2b488bc7e2f22d7e4620b42e29e
BZIvanov <ivanov.biser@gmx.com>
2021-03-15 21:51:40 +0200

    trainers demo project

---
+++ 12.Trainers-proj/src/components/ui/BaseBadge.vue
@@ -0,0 +1,39 @@
+<template>
+  <span class="badge" :class="type">
+    {{ text }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: ['type', 'title'],
+  computed: {
+    text() {
+      return this.title.toUpperCase();
+    }
+  }
+}
+</script>
+
+<style scoped>
+.badge {
+  background-color: #ddd;
+  color: #bbb;
+  border-radius: 28px;
+  padding: 0.5rem 1.4rem;
+  display: inline-block;
+  margin-right: 0.4rem;
+}
+.frontend {
+  background-color: #3d018d;
+  color: white;
+}
+.backend {
+  background-color: #71018d;
+  color: white;
+}
+.career {
+  background-color: #8d016e;
+  color: white;
+}
+</style>
---
+++ 12.Trainers-proj/src/components/ui/BaseButton.vue
@@ -0,0 +1,69 @@
+<template>
+  <button v-if="!link" :class="mode">
+    <slot></slot>
+  </button>
+  <router-link v-else :to="to" :class="mode">
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    mode: {
+      type: String,
+      required: false,
+      default: null
+    },
+    link: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    to: {
+      type: String,
+      required: false,
+      default: '/'
+    }
+  }
+}
+</script>
+
+<style scoped>
+button,
+a {
+  text-decoration: none;
+  padding: 0.7rem 1.4rem;
+  font: inherit;
+  background-color: #3a0161;
+  border: 1px solid #3a0161;
+  color: white;
+  cursor: pointer;
+  border-radius: 28px;
+  margin-right: 0.4rem;
+  display: inline-block;
+}
+a:hover,
+a:active,
+button:hover,
+button:active {
+  background-color: #270141;
+  border-color: #270141;
+}
+.flat {
+  background-color: transparent;
+  color: #3a0161;
+  border: none;
+}
+.outline {
+  background-color: transparent;
+  border-color: #270141;
+  color: #270141;
+}
+.flat:hover,
+.flat:active,
+.outline:hover,
+.outline:active {
+  background-color: #edd3ff;
+}
+</style>
