9a081562cdcc86c0e26ecd3316c794ebdcec4c7a
BZIvanov <biser.z.ivanov@gmail.com>
2021-03-16 13:29:25 +0200

    mixin demo

---
+++ 01.Basics/demo-e/src/components/AddUser.vue
@@ -0,0 +1,21 @@
+<template>
+  <user-alert v-if="alertIsVisible" title="Add User?" @close="hideAlert">
+    <p>Do you want to add user?</p>
+  </user-alert>
+  <section>
+    <h2>Add User</h2>
+    <button @click="showAlert">Add User</button>
+  </section>
+</template>
+
+<script>
+import UserAlert from './UserAlert.vue';
+import alertMixin from '../mixins/alert.js';
+
+export default {
+  components: {
+    UserAlert,
+  },
+  mixins: [alertMixin],
+};
+</script>
