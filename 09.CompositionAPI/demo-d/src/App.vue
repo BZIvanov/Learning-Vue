588688e22f1d744fcb9bea7dfdaeb64dd42ac085
BZIvanov <ivanov.biser@gmx.com>
2021-03-16 11:17:55 +0200

    props, provide and inject composition api demo

---
+++ 09.CompositionAPI/demo-d/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <user-data :first-name="firstName" :last-name="lastName"></user-data>
+</template>
+
+<script>
+import { ref, provide } from 'vue';
+import UserData from './components/UserData.vue';
+
+export default {
+  components: {
+    UserData,
+  },
+  setup() {
+    const firstName = ref('Mira');
+    const lastName = ref('Ivanova');
+    const age = ref(27);
+
+    provide('userAge', age);
+
+    return { firstName, lastName };
+  },
+};
+</script>
