06da6cf2717cff31b94ff9bd42da896e330d914d
BZIvanov <biser.z.ivanov@gmail.com>
2021-03-16 11:17:55 +0200

    props, provide and inject composition api demo

---
+++ 09.CompositionAPI/demo-d/src/components/UserData.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
+  </div>
+</template>
+
+<script>
+import { computed, inject } from 'vue';
+
+export default {
+  props: ['firstName', 'lastName'],
+  setup(props, context) {
+    const uName = computed(function() {
+      return props.firstName + ' ' + props.lastName;
+    });
+
+    // this is how we inject provided values, note that you should never mutate inject values, changes should be done on the provided value
+    const age = inject('userAge');
+
+    // we can use context for example to emit a value
+    console.log(context);
+
+    return { userName: uName, age };
+  },
+};
+</script>
