9faba8abf9412dc523df7fe4f9afa431270bf29d
BZIvanov <biser.z.ivanov@gmail.com>
2021-03-17 21:24:01 +0200

    custom hooks demo

---
+++ 09.CompositionAPI/demo-custom-hooks/src/components/projects/ProjectItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li>
+    <h3>{{ title }}</h3>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['title'],
+};
+</script>
+
+<style scoped>
+li {
+  background-color: #00017b;
+  color: white;
+  padding: 0.4rem;
+  margin: 0.5rem 0;
+}
+
+li h3 {
+  margin: 0;
+}
+</style>
---
+++ 09.CompositionAPI/demo-custom-hooks/src/components/users/UserItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h3>{{ userName }}</h3>
+    <button @click="viewProjects">View Projects</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['id', 'userName'],
+  emits: ['list-projects'],
+  methods: {
+    viewProjects() {
+      this.$emit('list-projects', this.id);
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  margin: 0.4rem 0;
+  padding: 1rem;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.27);
+  border: 1px solid #bbb;
+}
+
+li h3 {
+  margin: 0;
+}
+</style>
